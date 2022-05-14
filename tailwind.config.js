d032f663763f4d8b277f7a92ce69734170528a78
moise <mbakopmoise237@gmail.com>
2022-05-14 23:29:58 +0800

    fixing some error with media query

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 module.exports = {
-  content: ["./dist/index.html"],
-  theme: {
-    extend: {
-      container:{
-        padding: "6rem"
-      }
-    },
-  },
-  plugins: [],
-}
+	content: ['./dist/index.html'],
+	theme: {
+		extend: {
+			container: {
+				padding: '6rem',
+			},
+			screens: {
+				sm: '360px',
+				// => @media (min-width: 640px) { ... }
+
+				md: '799px',
+				// => @media (min-width: 768px) { ... }
+
+				lg: '1024px',
+				// => @media (min-width: 1024px) { ... }
+
+				xl: '1280px',
+				// => @media (min-width: 1280px) { ... }
+
+				'2xl': '1536px',
+				// => @media (min-width: 1536px) { ... }
+			},
+		},
+	},
+	plugins: [],
+};
